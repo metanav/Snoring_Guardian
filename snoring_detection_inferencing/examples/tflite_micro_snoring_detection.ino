c4c18c1fad36dd915bacdd971a9f376f379409d2
Naveen Kumar <>
2021-07-19 13:42:50 +0900

    Added Arduino Library

---
+++ snoring_detection_inferencing/examples/tflite_micro_snoring_detection.ino
@@ -0,0 +1,301 @@
+// If your target is limited in memory remove this macro to save 10K RAM
+#define EIDSP_QUANTIZE_FILTERBANK   0
+
+/**
+   Define the number of slices per model window. E.g. a model window of 1000 ms
+   with slices per model window set to 4. Results in a slice size of 250 ms.
+   For more info: https://docs.edgeimpulse.com/docs/continuous-audio-sampling
+*/
+#define EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW 3
+
+/* Includes ---------------------------------------------------------------- */
+#include <PDM.h>
+#include <Scheduler.h>
+#include <RingBuf.h>
+#include <snore_detection_inferencing.h>
+
+/** Audio buffers, pointers and selectors */
+typedef struct {
+  signed short *buffers[2];
+  unsigned char buf_select;
+  unsigned char buf_ready;
+  unsigned int buf_count;
+  unsigned int n_samples;
+} inference_t;
+
+static inference_t inference;
+static bool record_ready = false;
+static signed short *sampleBuffer;
+static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
+static int print_results = -(EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW);
+
+bool alert = false;
+
+RingBuf<uint8_t, 10> last_ten_predictions;
+int greenLED = 23;
+int vibratorPin = 3;   // Vibration motor connected to D3 PWM pin
+bool is_motor_running = false;
+
+void run_vibration()
+{
+  if (alert)
+  {
+    is_motor_running = true;
+
+    for (int i = 0; i < 2; i++)
+    {
+      analogWrite(vibratorPin, 30);
+      delay(1000);
+      analogWrite(vibratorPin, 0);
+      delay(1500);
+    }
+    
+    is_motor_running = false;
+  } else {
+    if (is_motor_running)
+    {
+      analogWrite(vibratorPin, 0);
+    }
+  }
+  yield();
+}
+
+
+
+/**
+   @brief      Printf function uses vsnprintf and output using Arduino Serial
+
+   @param[in]  format     Variable argument list
+*/
+void ei_printf(const char *format, ...) {
+  static char print_buf[1024] = { 0 };
+
+  va_list args;
+  va_start(args, format);
+  int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
+  va_end(args);
+
+  if (r > 0) {
+    Serial.write(print_buf);
+  }
+}
+
+/**
+   @brief      PDM buffer full callback
+               Get data and call audio thread callback
+*/
+static void pdm_data_ready_inference_callback(void)
+{
+  int bytesAvailable = PDM.available();
+
+  // read into the sample buffer
+  int bytesRead = PDM.read((char *)&sampleBuffer[0], bytesAvailable);
+
+  if (record_ready == true) {
+    for (int i = 0; i<bytesRead >> 1; i++) {
+      inference.buffers[inference.buf_select][inference.buf_count++] = sampleBuffer[i];
+
+      if (inference.buf_count >= inference.n_samples) {
+        inference.buf_select ^= 1;
+        inference.buf_count = 0;
+        inference.buf_ready = 1;
+      }
+    }
+  }
+}
+
+/**
+   @brief      Init inferencing struct and setup/start PDM
+
+   @param[in]  n_samples  The n samples
+
+   @return     { description_of_the_return_value }
+*/
+static bool microphone_inference_start(uint32_t n_samples)
+{
+  inference.buffers[0] = (signed short *)malloc(n_samples * sizeof(signed short));
+
+  if (inference.buffers[0] == NULL) {
+    return false;
+  }
+
+  inference.buffers[1] = (signed short *)malloc(n_samples * sizeof(signed short));
+
+  if (inference.buffers[0] == NULL) {
+    free(inference.buffers[0]);
+    return false;
+  }
+
+  sampleBuffer = (signed short *)malloc((n_samples >> 1) * sizeof(signed short));
+
+  if (sampleBuffer == NULL) {
+    free(inference.buffers[0]);
+    free(inference.buffers[1]);
+    return false;
+  }
+
+  inference.buf_select = 0;
+  inference.buf_count = 0;
+  inference.n_samples = n_samples;
+  inference.buf_ready = 0;
+
+  // configure the data receive callback
+  PDM.onReceive(&pdm_data_ready_inference_callback);
+
+  PDM.setBufferSize((n_samples >> 1) * sizeof(int16_t));
+
+  // initialize PDM with:
+  // - one channel (mono mode)
+  // - a 16 kHz sample rate
+  if (!PDM.begin(1, EI_CLASSIFIER_FREQUENCY)) {
+    ei_printf("Failed to start PDM!");
+  }
+
+  // set the gain, defaults to 20
+  PDM.setGain(127);
+
+  record_ready = true;
+
+  return true;
+}
+
+/**
+   @brief      Wait on new data
+
+   @return     True when finished
+*/
+static bool microphone_inference_record(void)
+{
+  bool ret = true;
+
+  if (inference.buf_ready == 1) {
+    ei_printf(
+      "Error sample buffer overrun. Decrease the number of slices per model window "
+      "(EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW)\n");
+    ret = false;
+  }
+
+  while (inference.buf_ready == 0) {
+    delay(1);
+  }
+
+  inference.buf_ready = 0;
+
+  return ret;
+}
+
+/**
+   Get raw audio signal data
+*/
+static int microphone_audio_signal_get_data(size_t offset, size_t length, float * out_ptr)
+{
+  numpy::int16_to_float(&inference.buffers[inference.buf_select ^ 1][offset], out_ptr, length);
+
+  return 0;
+}
+
+/**
+   @brief      Stop PDM and release buffers
+*/
+static void microphone_inference_end(void)
+{
+  PDM.end();
+  free(inference.buffers[0]);
+  free(inference.buffers[1]);
+  free(sampleBuffer);
+}
+
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(greenLED, OUTPUT);
+  pinMode(greenLED, LOW); // HIGH is off, LOW is on
+  pinMode(vibratorPin, OUTPUT);  // sets the pin as output
+
+  // summary of inferencing settings (from model_metadata.h)
+  ei_printf("Inferencing settings:\n");
+  ei_printf("\tInterval: %.2f ms.\n", (float)EI_CLASSIFIER_INTERVAL_MS);
+  ei_printf("\tFrame size: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+  ei_printf("\tSample length: %d ms.\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT / 16);
+  ei_printf("\tNo. of classes: %d\n", sizeof(ei_classifier_inferencing_categories) /
+            sizeof(ei_classifier_inferencing_categories[0]));
+
+  run_classifier_init();
+  if (microphone_inference_start(EI_CLASSIFIER_SLICE_SIZE) == false) {
+    ei_printf("ERR: Failed to setup audio sampling\r\n");
+    return;
+  }
+
+  Scheduler.startLoop(run_vibration);
+}
+
+void loop()
+{
+
+  bool m = microphone_inference_record();
+
+  if (!m) {
+    ei_printf("ERR: Failed to record audio...\n");
+    return;
+  }
+
+  signal_t signal;
+  signal.total_length = EI_CLASSIFIER_SLICE_SIZE;
+  signal.get_data = &microphone_audio_signal_get_data;
+  ei_impulse_result_t result = {0};
+
+  EI_IMPULSE_ERROR r = run_classifier_continuous(&signal, &result, debug_nn);
+  if (r != EI_IMPULSE_OK) {
+    ei_printf("ERR: Failed to run classifier (%d)\n", r);
+    return;
+  }
+
+  if (++print_results >= (EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW)) {
+    // print the predictions
+    ei_printf("Predictions ");
+    ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
+              result.timing.dsp, result.timing.classification, result.timing.anomaly);
+    ei_printf(": \n");
+
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+      ei_printf("    %s: %.5f\n", result.classification[ix].label,
+                result.classification[ix].value);
+
+      if (ix == 1 && !is_motor_running && result.classification[ix].value > 0.9) {
+        if (last_ten_predictions.isFull()) {
+          uint8_t k;
+          last_ten_predictions.pop(k);
+        }
+
+        last_ten_predictions.push(ix);
+
+        uint8_t count = 0;
+
+        for (uint8_t j = 0; j < last_ten_predictions.size(); j++) {
+          count += last_ten_predictions[j];
+          //ei_printf("%d, ", last_ten_predictions[j]);
+        }
+        //ei_printf("\n");
+        ei_printf("Snoring\n");
+        pinMode(greenLED, HIGH); // LOW is on
+        if (count >= 5) {
+          ei_printf("Trigger vibration motor\n");
+          alert = true;
+        }
+      }  else {
+        ei_printf("Noise\n");
+        pinMode(greenLED, LOW); // HIGH is off
+        alert = false;
+      }
+
+      print_results = 0;
+    }
+  }
+}
+
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_MICROPHONE
+#error "Invalid model for current sensor."
+#endif
